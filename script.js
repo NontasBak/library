bad8202790b710b041bcd73aafe7e0c7c362cbea
NontasBak <nontasbak@gmail.com>
2023-12-01 11:48:29 +0200

    Allow user to add books in the library

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ function Book(title, author, pages, read) {
     }
 }
 
-let newBook = new Book("bla", "blabla", 200, true);
-console.log(newBook.info());
+// let newBook = new Book("bla", "blabla", 200, true);
+// console.log(newBook.info());
 
+const myLibrary = [];
+
+const submitButton = document.querySelector("#add-book");
+submitButton.addEventListener("click", addBookToLibrary);
+
+function addBookToLibrary() {
+    const bookInputs = document.querySelectorAll("input");
+    myLibrary.push(new Book(bookInputs[0].value, bookInputs[1].value, bookInputs[2].value, bookInputs[3].checked));
+    console.log(myLibrary);
+}
