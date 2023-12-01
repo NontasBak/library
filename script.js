f4002fd408afdef7b0068eaecf0e477900dec143
NontasBak <nontasbak@gmail.com>
2023-12-01 18:52:37 +0200

    Update Library output when new book is added

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
 
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? "read" : "not read yet"}`
-    }
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? "read" : "not read yet"}`
 }
 
 // let newBook = new Book("bla", "blabla", 200, true);
@@ -18,26 +18,45 @@ let myLibrary = [new Book("1-1", "1-2", "1-3", true), new Book("2-1", "2-2", "2-
 console.log(myLibrary)
 displayLibrary();
 
-const submitButton = document.querySelector("#add-book");
-submitButton.addEventListener("click", addBookToLibrary);
+const form = document.querySelector("form");
+form.addEventListener("submit", addBookToLibrary);
 
 
 
-
-
-function addBookToLibrary() {
+function addBookToLibrary(event) {
+    event.preventDefault();
     const bookInputs = document.querySelectorAll("input");
     myLibrary.push(new Book(bookInputs[0].value, bookInputs[1].value, bookInputs[2].value, bookInputs[3].checked));
-    console.log(myLibrary);
+    // console.log(myLibrary);
 
     updateLibraryDisplay();
 }
 
-// function updateLibraryDisplay() {
-//     const bookContainer = document.querySelector(".book-container");
-    
+function updateLibraryDisplay() {
+    const bookContainer = document.querySelector(".book-container");
+    let lastBook = document.createElement("div");
+    lastBook.classList.add("book");
+    let lastBookInLibrary = myLibrary[myLibrary.length - 1];
+    console.log(lastBookInLibrary);
 
-// }
+    for(let property in lastBookInLibrary)
+    {
+        if(property === "info")
+            continue;
+        
+        let bookInfo = document.createElement("p");
+        if(!(property === "read")) {
+            bookInfo.textContent = lastBookInLibrary[property];
+        }
+        else {
+            bookInfo.textContent = lastBookInLibrary[property] ? "read" : "not read";
+        }
+
+        lastBook.appendChild(bookInfo);
+    }
+
+    bookContainer.appendChild(lastBook);
+}
 
 function displayLibrary() {
     const libraryContainer = document.querySelector(".library-container");
@@ -46,34 +65,26 @@ function displayLibrary() {
 
     myLibrary.forEach((book) => {
         let specificBook = document.createElement("div");
+        specificBook.classList.add("book");
         for(let property in book)
         {
-            let bookInfo = document.createElement("p");
-            // console.log(property);
-
             if(property === "info")
                 continue;
             
+            let bookInfo = document.createElement("p");
             if(!(property === "read")) {
                 bookInfo.textContent = book[property];
-                // console.log(bookInfo.textContent);
             }
             else {
                 bookInfo.textContent = book[property] ? "read" : "not read";
-                // console.log(bookInfo.textContent);
             }
 
             specificBook.appendChild(bookInfo);
-            // console.log("works")
         }
-        specificBook.classList.add("book");
+        
         bookContainer.appendChild(specificBook);
     })
 
     libraryContainer.appendChild(bookContainer);
       
 }
-
-// createBookDisplay(book, bookContainer) {
-
-// }
